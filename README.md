fe3ea74a22188c974c0420bec1c6af32375801de
fsj <47785500+hhhfsj@users.noreply.github.com>
2021-11-14 17:17:45 +0800

    Add files via upload
    第一次提交，写的很烂

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-# plugin-template
+# Blessing Skin Server live2d
 
-This is a template repository for creating plugin, feel free to use it then modify it.
+可在Blessing Skin Server内加载live2d
+本插件不包含任何模型
+
+### 需在版权文字区添加以下信息：
+
+```
+<div id="landlord" style="background-color: transparent;" >
+    <canvas id="live2d" width="200" height="250" class="live2d" style="background-color: transparent;"></canvas>
+</div>
+```
+
+### 自定义js添加：
+
+```
+loadlive2d("live2d", "/plugins/pigeon-skin/assets/live2d/model/Suzukaze-Aoba/Aoba.json"); //模型地址
+var home_Path = 'https://skin.fsj-mc.club/' //模型链接域名
+```
+
 
 ## License
 
--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,27 @@
 <?php
 
-use Blessing\Filter;
+use App\Services\Hook;
+use App\Services\Plugin;
+use App\Events\RenderingHeader;
 use Illuminate\Contracts\Events\Dispatcher;
 
-return function (Dispatcher $events, Filter $filter) {
-    //
+return function (Plugin $plugin) {
+    //     加载资源(live 2d)
+    Hook::addStyleFileToPage(
+        $plugin->assets('live2d/css/live2d.css') // 样式
+    );
+    Hook::addScriptFileToPage(
+        $plugin->assets('live2d/js/live2d.js') // js
+    );
+    // 聊天信息 按需启用
+    // Hook::addScriptFileToPage(
+    //     $plugin->assets('live2d/js/message1.js') // 聊天
+    // );
+    Hook::addScriptFileToPage(
+        $plugin->assets('jquery-ui.min.js?v=1.12.1') // Jquery UI
+    );
+//      加载资源（特效）
+    Hook::addScriptFileToPage(
+        $plugin->assets('Effects/Mouse effects.js')
+    );
 };
